3cee4a45839732f663f3be2bcb29ba51d1f5d6a6
NaNameUz3r <f4ckj00gl3@gmail.com>
2021-07-25 16:01:22 +0300

    add check_balance function with its tests

--- stack/check_balance.py
+++ stack/check_balance.py
@@ -1,4 +1,16 @@
 from stack_reversed import Stack
 
-def check_balance(parentheses_string):
-    pass
+
+def is_balanced(parentheses_string):
+    checking_stack = Stack()
+    for parenthesis in parentheses_string:
+        if parenthesis == "(":
+            checking_stack.push('(')
+        elif parenthesis == ")" and checking_stack.peek() == "(":
+            checking_stack.pop()
+
+    return checking_stack.size() == 0
+
+
+# print(is_balanced("(())()(())"))
+print(is_balanced('()((())()))'))
---
+++ stack/check_balance_tests.py
@@ -0,0 +1,15 @@
+from check_balance import is_balanced
+import unittest
+
+class CheckBalanceTests(unittest.TestCase):
+
+    def test_balance(self):
+        string_1 = '(())()(())'     # True
+        string_2 = '(()((())()))'   # True
+        string_3 = '))(('           # False
+        string_4 = '((())'          # False
+
+        self.assertEqual(is_balanced(string_1), True)
+        self.assertEqual(is_balanced(string_2), True)
+        self.assertEqual(is_balanced(string_3), False)
+        self.assertEqual(is_balanced(string_4), False)
