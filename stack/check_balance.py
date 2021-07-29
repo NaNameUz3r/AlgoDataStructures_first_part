ca9937d84ea2a1695d9bbb5bdec232cc5c561951
NaNameUz3r <f4ckj00gl3@gmail.com>
2021-07-29 10:10:11 +0300

    make it shorter. all test are passed

--- stack/check_balance.py
+++ stack/check_balance.py
@@ -4,9 +4,7 @@ from stack_reversed import Stack
 def is_balanced(parentheses_string):
     checking_stack = Stack()
     for parenthesis in parentheses_string:
-        if parenthesis == "(":
-            checking_stack.push(parenthesis)
-        elif parenthesis == ")" and checking_stack.peek() == "(":
+        if parenthesis == ")" and checking_stack.peek() == "(":
             checking_stack.pop()
         else:
             checking_stack.push(parenthesis)
