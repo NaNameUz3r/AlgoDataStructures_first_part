75dd311ee3845eea6d069a9a4c029852e8286af8
NaNameUz3r <f4ckj00gl3@gmail.com>
2021-07-29 10:32:15 +0300

    change implementation, now O(N) is only when second stack is empty, other operations is O(1)

--- Queue/two_stacks_queue.py
+++ Queue/two_stacks_queue.py
@@ -7,18 +7,15 @@ class TwoStackQueue:
         self.second_stack = Stack()
 
     def enqueue(self, item):
-
-        while self.first_stack.size() > 0:
-            self.second_stack.push(self.first_stack.pop())
-
         self.first_stack.push(item)
 
-        while self.second_stack.size() > 0:
-            self.first_stack.push(self.second_stack.pop())
-
     def dequeue(self):
-        if self.size() > 0:
-            return self.first_stack.pop()
+        if self.second_stack.size() == 0:
+            while self.first_stack.size() > 0:
+                self.second_stack.push(self.first_stack.pop())
+
+        if self.second_stack.size() > 0:
+            return self.second_stack.pop()
         else:
             return None
 
