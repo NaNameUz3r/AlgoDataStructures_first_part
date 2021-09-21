a0879980ca6a494a46e0ab5946251be951a3de4d
NaNameUz3r <f4ckj00gl3@gmail.com>
2021-09-21 10:04:06 +0300

    add Bloom filter study realization and tests

---
+++ BloomFilter/bloom_filter.py
@@ -0,0 +1,36 @@
+class BloomFilter:
+
+    def __init__(self, f_len):
+        self.filter_len = f_len
+        self.bit_map = [0] * self.filter_len
+
+    def hash1(self, str1):
+        hashed_index = 0
+        hardcoded_random = 17
+        for c in str1:
+            code = ord(c)
+            hashed_index = hashed_index * hardcoded_random + code
+        hashed_index = hashed_index % self.filter_len
+        return hashed_index
+
+    def hash2(self, str1):
+        hashed_index = 0
+        hardcoded_random = 223
+        for c in str1:
+            code = ord(c)
+            hashed_index = hashed_index * hardcoded_random + code
+        hashed_index = hashed_index % self.filter_len
+        return hashed_index
+
+    def add(self, str1):
+        hashed_indexes = [self.hash1(str1), self.hash2(str1)]
+        for index in hashed_indexes:
+            self.bit_map[index] = 1
+
+    def is_value(self, str1):
+        hashed_indexes = [self.hash1(str1), self.hash2(str1)]
+        total_bits = 0
+        for index in hashed_indexes:
+            if self.bit_map[index] == 1:
+                total_bits += 1
+        return total_bits == len(hashed_indexes)
---
+++ BloomFilter/bloom_filter_tests.py
@@ -0,0 +1,22 @@
+import unittest
+from bloom_filter import BloomFilter
+
+class TestBloomFilter(unittest.TestCase):
+
+    def test_bloom(self):
+        bf = BloomFilter(32)
+
+        bf.add("0123456789")
+        bf.add("1234567890")
+        bf.add("2345678901")
+        bf.add("3456789012")
+        bf.add("4567890123")
+        bf.add("5678901234")
+        bf.add("6789012345")
+        bf.add("7890123456")
+        bf.add("8901234567")
+        bf.add("9012345678")
+
+        self.assertEqual(bf.is_value('9012345678'), True)
+        self.assertEqual(bf.is_value("1234567890"), True)
+        self.assertEqual(bf.is_value("0187654322"), False)
